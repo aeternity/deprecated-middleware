edb3eeb342d785187ff6c2100c94a102486a8bfc
Till Kolter <till.kolter@gmail.com>
2018-03-12 17:31:01 +0100

    Add routes

--- epoch_extra/urls.py
+++ epoch_extra/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.routers import DefaultRouter
 
-from epoch_extra.views import BalanceView
+from epoch_extra.views import BalanceView, AensViewSet
 
-# router = DefaultRouter()
+router = DefaultRouter()
+
+router.register(r'^name', AensViewSet, base_name='name')
+
 
 urlpatterns = [
-    url(r'^balance/(?P<pk>.+)', csrf_exempt(BalanceView.as_view({'get': 'retrieve'})))
-]
+    url(r'^balance/(?P<pk>.+)', csrf_exempt(BalanceView.as_view({'get': 'retrieve'}))),
+    url(r'<')
+] + router.urls
